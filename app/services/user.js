4d7d7e9cbd6e6d9f2d43648e1e1e597688f00513
Mikael Viitaniemi <mikael.viitaniemi@futurice.com>
2015-11-17 07:47:00 +0200

    Fixed related resource methods for User

--- app/services/user.js
+++ app/services/user.js
@@ -2,45 +2,26 @@
 
 /* global angular */
 
-var User = angular.module('User', ['ngResource', 'Media']);
+var User = angular.module('User', ['ngResource', 'Media', 'Playlist', 'Device']);
 
 User.factory('User', ['$resource', 'Media', 'Playlist', 'Device',
 function ($resource, Media, Playlist, Device) {
-  return $resource('/api/user/:username', {
+  var resource = $resource('/api/user/:username', {
     username: '@username'
-  }, {
-    getMedia: {
-      method: 'GET',
-      url: '/api/user/:username/media',
-      isArray: true,
-      transformResponse: function (data) {
-        var media = angular.fromJson(data);
-        return media.map(function (mediaObject) {
-          return new Media(mediaObject);
-        });
-      }
+  });
+
+
+  angular.extend(resource.prototype, {
+    getMedia: function() {
+      return Media.query({username: this.username});
     },
-    getPlaylists: {
-      method: 'GET',
-      url: '/api/user/:username/playlist',
-      isArray: true,
-      transformResponse: function (data) {
-        var playlist = angular.fromJson(data);
-        return playlist.map(function (playlistObject) {
-          return new Playlist(playlistObject);
-        });
-      }
+    getPlaylists: function() {
+      return Playlist.query({username: this.username});
     },
-    getDevices: {
-      method: 'GET',
-      url: '/api/user/:username/device',
-      isArray: true,
-      transformResponse: function (data) {
-        var device = angular.fromJson(data);
-        return device.map(function (deviceObject) {
-          return new Device(deviceObject);
-        });
-      }
+    getDevices: function() {
+      return Device.query({username: this.username});
     }
   });
+
+  return resource;
 }]);
