7eadc08773ed275f6c541991d800809a1309ca8b
Hannu Lahtinen <hannu.lahtinen@student.tut.fi>
2015-12-20 19:30:37 +0200

    Had forgotten this decorator import from last commit

--- hisra_models/views.py
+++ hisra_models/views.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from django.conf import settings
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
