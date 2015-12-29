16d11a11ab47bf7eb1a6cf1aaf084feb492bf10c
Daniel <daniel.rajala@gmail.com>
2015-12-29 22:00:02 +0200

    Made playlists editable, unfinished

--- hisra_webapp/src/app/index.module.js
+++ hisra_webapp/src/app/index.module.js
@@ -14,6 +14,7 @@
       'ngRoute',
       'ngMaterial',
       'toastr',
-      'ngResource']);
+      'ngResource',
+      'ui.sortable']);
 
 })();
--- hisra_webapp/src/app/playlist-detail/playlist-detail.controller.js
+++ hisra_webapp/src/app/playlist-detail/playlist-detail.controller.js
@@ -5,13 +5,37 @@ angular.module('hisraWebapp')
 .controller('PlaylistDetailController', PlaylistDetailController);
 
 /* @ngInject */
-function PlaylistDetailController($location, $routeParams, Authentication, User, Playlist) {
+function PlaylistDetailController($scope, $location, $routeParams, Authentication, User, Playlist) {
   var user = Authentication.getCurrentUser();
-  if(user == undefined) {
+  if(user === undefined) {
       return $location.path('/login');
   }
 
   var vm = this;
+  $scope.items = ["one", "two", "thre", "four", "five", "six"];
+  $scope.types = ['web_page', 'video', 'image'];
+  $scope.selected = undefined;
+  $scope.getTemplate = function (media) {
+        if($scope.selected === undefined) return 'display';
+       if (media.uri === $scope.selected.uri) return 'edit';
+       else return 'display';
+   };
+
+   $scope.editMedia = function (media) {
+     console.dir(media);
+        $scope.selected = angular.copy(media);
+    };
+
+    $scope.saveMedia = function (index) {
+        console.log("Saving contact");
+        vm.playlist.media_schedule[index] = angular.copy($scope.selected);
+        $scope.reset();
+    };
+
+    $scope.reset = function () {
+        $scope.selected = undefined;
+    };
+
   Playlist.get({
       id: $routeParams.playlistId,
       username: user.username
@@ -22,5 +46,4 @@ function PlaylistDetailController($location, $routeParams, Authentication, User,
         vm.playlist = playlist;
     });
 }
-
 })();
