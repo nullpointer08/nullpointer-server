fb26e7939b0188124ff9a348400a5ae930a1e2f5
Mikael <xarg@precise>
2016-01-05 05:47:20 +0200

    Make chunked_upload_complete @csrf_exempt Skip md5 check from browserside
    Add missing headers to CORS allowed list for chunked upload
    
    Made media controller work with chunked upload to a further extent

--- hisra_webapp/src/app/media/media.controller.js
+++ hisra_webapp/src/app/media/media.controller.js
@@ -5,7 +5,7 @@
   .controller('MediaController', MediaController);
 
   /* @ngInject */
-  function MediaController(Authentication, $location, User, Media, BASE_URL) {
+  function MediaController($http, Authentication, $location, User, Media, BASE_URL) {
 
     var user = Authentication.getCurrentUser();
     if(user == undefined) {
@@ -16,7 +16,6 @@
     vm.media = [];
     vm.BASE_URL = BASE_URL;
 
-    // TODO: Get username from auth service
     User.getMedia({username: user.username}).$promise
       .then(function (media) {
         vm.media = media;
@@ -36,6 +35,7 @@
     };
 
     $('#media-add__file').fileupload({
+      //maxChunkSize: 100,
       url: BASE_URL + '/api/chunked_upload/',
       autoUpload: true,
       maxNumberOfFiles: 1,
@@ -43,7 +43,14 @@
         xhr.setRequestHeader('Authorization', 'Basic ' + user.authdata);
       },
       done: function(e, data) {
-        console.log(data);
+        console.log(e, data);
+        $http.post(BASE_URL + '/api/chunked_upload_complete', $.param({
+          "upload_id": data.result.upload_id
+        }), {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
       }
     })
   }
