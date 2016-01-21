edcb7ce56486ba9fd0fb8ff109ce87a3d741bb08
Sami Surakka <sami.surakka@gmail.com>
2016-01-21 15:31:21 +0200

    Visual fixes vol. 01

--- hisra_webapp/src/app/login/login.controller.js
+++ hisra_webapp/src/app/login/login.controller.js
@@ -8,12 +8,19 @@ angular.module('hisraWebapp')
 /* @ngInject */
 function LoginController(Authentication, $location) {
   	var vm = this;
+    
+    activate(); // Log the user out if she navigates to the login screen?
+    
 	vm.login = function(user) {
       Authentication.login(user.username, user.password)
       	.then(function() {
           $location.path('/');
       	});
   };
+  
+  function activate() {
+      Authentication.logout();
+  }
 }
 
 })();
